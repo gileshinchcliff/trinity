c5cb54dc2061199581329b16cf167bd2dcb5fde1
Giles Hinchcliff <giles@rebelastronaut.co.uk>
2020-02-15 19:05:48 +0000

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -152,9 +152,6 @@ module.exports = {
     `gatsby-plugin-netlify-cms`,
     'gatsby-plugin-netlify-identity-widget',
     `gatsby-plugin-netlify-cms-paths`,
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,

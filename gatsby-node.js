7edec69dd2eebcb9c9b0de00a02206d6a00fa20b
Giles Hinchcliff <giles@rebelastronaut.co.uk>
2020-02-18 13:21:24 +0000

    add stuff

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,126 +1,212 @@
-const { createRemoteFileNode } = require(`gatsby-source-filesystem`);
-const get = require('lodash/get');
+// const { createRemoteFileNode } = require(`gatsby-source-filesystem`);
+// const get = require('lodash/get');
 
-console.log("is this working?")
-exports.onCreateNode = async (
-    { node, actions, store, cache, createNodeId },
-    options
-) => {
-    const { createNode } = actions;
-    const {
-        nodeType = 'GoogleCalendarEvent',
-        imagePath = 'nodes.attachments[].fileUrl',
-        name = 'localImage',
-        auth = {},
-        ext = null,
-        prepareUrl = null,
-    } = options;
-    const createImageNodeOptions = {
-        store,
-        cache,
-        createNode,
-        createNodeId,
-        auth,
-        ext,
-        name,
-        prepareUrl,
-    };
-    console.log(node.internal.type)
-    if (node.internal.type === nodeType) {
-        console.log(node.internal.type)
-        // Check if any part of the path indicates the node is an array and splits at those indicators
-        let imagePathSegments = [];
-        if (imagePath.includes('[].')) {
-            imagePathSegments = imagePath.split('[].');
+// console.log("is this working?")
+// exports.onCreateNode = async (
+//     { node, actions, store, cache, createNodeId },
+//     options
+// ) => {
+//     const { createNode } = actions;
+//     const {
+//         nodeType = 'GoogleCalendarEvent',
+//         imagePath = 'nodes.attachments[].fileUrl',
+//         name = 'localImage',
+//         auth = {},
+//         ext = null,
+//         prepareUrl = null,
+//     } = options;
+//     const createImageNodeOptions = {
+//         store,
+//         cache,
+//         createNode,
+//         createNodeId,
+//         auth,
+//         ext,
+//         name,
+//         prepareUrl,
+//     };
+//     console.log(node.internal.type)
+//     if (node.internal.type === nodeType) {
+//         console.log(node.internal.type)
+//         // Check if any part of the path indicates the node is an array and splits at those indicators
+//         let imagePathSegments = [];
+//         if (imagePath.includes('[].')) {
+//             imagePathSegments = imagePath.split('[].');
+//         }
+//         console.log(imagePathSegments)
+//         if (imagePathSegments.length) {
+//             await createImageNodesInArrays(imagePathSegments[0], node, {
+//                 imagePathSegments,
+//                 ...createImageNodeOptions,
+//             });
+//         } else {
+//             const url = getPath(node, imagePath, ext);
+//             await createImageNode(url, node, createImageNodeOptions);
+//         }
+//     }
+// };
+
+// // Returns value from path, adding extension when supplied
+// function getPath(node, path, ext = null) {
+//     const value = get(node, path);
+//     console.log(value)
+//     return ext ? value + ext : value;
+// }
+
+// // Creates a file node and associates the parent node to its new child
+// async function createImageNode(url, node, options) {
+//     console.log(url)
+//     console.log(node)
+//     console.log(options)
+//     const { name, imagePathSegments, prepareUrl, ...restOfOptions } = options;
+//     let fileNode;
+
+//     if (!url) {
+//         return;
+//     }
+
+//     if (typeof prepareUrl === 'function') {
+//         url = prepareUrl(url);
+//     }
+//     console.log(url)
+//     try {
+//         fileNode = await createRemoteFileNode({
+//             ...restOfOptions,
+//             url,
+//             parentNodeId: node.id,
+//         });
+//     } catch (e) {
+//         console.error('gatsby-plugin-remote-images ERROR:', e);
+//     }
+//     // Adds a field `localImage` or custom name to the node
+//     // ___NODE appendix tells Gatsby that this field will link to another node
+//     if (fileNode) {
+//         node[`${name}___NODE`] = fileNode.id;
+//     }
+// }
+
+// // Recursively traverses objects/arrays at each path part, then operates on targeted leaf node
+// async function createImageNodesInArrays(path, node, options) {
+//     if (!path || !node) {
+//         return;
+//     }
+//     const { imagePathSegments, ext } = options;
+//     const pathIndex = imagePathSegments.indexOf(path),
+//         isPathToLeafProperty = pathIndex === imagePathSegments.length - 1,
+//         nextValue = getPath(node, path, isPathToLeafProperty ? ext : null);
+//     // attachments = node.attachments
+//     // attachments.map( item =>
+//     //     console.log(item.fileUrl))
+
+//     // grab the parent of the leaf property, if it's not the current value of `node` already
+//     // ex: `parentNode` in `myNodes[].parentNode.leafProperty`
+//     let nextNode = node;
+//     if (isPathToLeafProperty && path.includes('.')) {
+//         const pathToLastParent = path
+//             .split('.')
+//             .slice(0, -1)
+//             .join('.');
+//         nextNode = get(node, pathToLastParent);
+//     }
+//     // console.log(nextNode)
+//     return Array.isArray(nextValue)
+//         ? // Recursively call function with next path segment for each array element
+//         Promise.all(
+//             nextValue.map(item =>
+//                 createImageNodesInArrays(
+//                     imagePathSegments[pathIndex + 1],
+//                     item,
+//                     options
+//                 ),
+//                 // console.log("running function")
+//             ),
+//         )
+//         : // otherwise, handle leaf node
+//         createImageNode(nextValue, nextNode, options);
+// }
+const path = require('path');
+const _ = require('lodash');
+const { fmImagesToRelative } = require('gatsby-remark-relative-images');
+// graphql function doesn't throw an error so we have to check to check for the result.errors to throw manually
+const wrapper = promise =>
+    promise.then(result => {
+        if (result.errors) {
+            throw result.errors;
         }
-        console.log(imagePathSegments)
-        if (imagePathSegments.length) {
-            await createImageNodesInArrays(imagePathSegments[0], node, {
-                imagePathSegments,
-                ...createImageNodeOptions,
-            });
-        } else {
-            const url = getPath(node, imagePath, ext);
-            await createImageNode(url, node, createImageNodeOptions);
+        return result;
+    })
+
+exports.onCreateNode = ({ node, actions }) => {
+    const { createNodeField } = actions;
+    let slug;
+    // Search for MDX filenodes
+    if (node.internal.type === 'Mdx') {
+        // If the frontmatter has a "slug", use it
+        if (
+            Object.prototype.hasOwnProperty.call(node, 'frontmatter') &&
+            Object.prototype.hasOwnProperty.call(node.frontmatter, 'slug')
+        ) {
+            slug = `/${_.kebabCase(node.frontmatter.slug)}`
         }
+        // If not derive a slug from the "title" in the frontmatter
+        else if (
+            Object.prototype.hasOwnProperty.call(node, 'frontmatter') &&
+            Object.prototype.hasOwnProperty.call(node.frontmatter, 'title')
+        ) {
+            slug = `/${_.kebabCase(node.frontmatter.title)}`
+        }
+        createNodeField({ node, name: 'slug', value: slug })
     }
-};
-
-// Returns value from path, adding extension when supplied
-function getPath(node, path, ext = null) {
-    const value = get(node, path);
-    console.log(value)
-    return ext ? value + ext : value;
+    fmImagesToRelative(node);
 }
 
-// Creates a file node and associates the parent node to its new child
-async function createImageNode(url, node, options) {
-    console.log(url)
-    console.log(node)
-    console.log(options)
-    const { name, imagePathSegments, prepareUrl, ...restOfOptions } = options;
-    let fileNode;
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions;
 
-    if (!url) {
-        return;
-    }
+    const result = await wrapper(
+        graphql(`
+        {
+          allMarkdownRemark {
+            edges {
+              node {
+                id
+                fileAbsolutePath
+                frontmatter {
+                  title
+                  date
+                }
+              }
+            }
+          }
+        }
+    `)
+    );
 
-    if (typeof prepareUrl === 'function') {
-        url = prepareUrl(url);
-    }
-    console.log(url)
-    try {
-        fileNode = await createRemoteFileNode({
-            ...restOfOptions,
-            url,
-            parentNodeId: node.id,
-        });
-    } catch (e) {
-        console.error('gatsby-plugin-remote-images ERROR:', e);
-    }
-    // Adds a field `localImage` or custom name to the node
-    // ___NODE appendix tells Gatsby that this field will link to another node
-    if (fileNode) {
-        node[`${name}___NODE`] = fileNode.id;
-    }
-}
+    const projectPosts = result.data.allMarkdownRemark.edges
 
-// Recursively traverses objects/arrays at each path part, then operates on targeted leaf node
-async function createImageNodesInArrays(path, node, options) {
-    if (!path || !node) {
-        return;
-    }
-    const { imagePathSegments, ext } = options;
-    const pathIndex = imagePathSegments.indexOf(path),
-        isPathToLeafProperty = pathIndex === imagePathSegments.length - 1,
-        nextValue = getPath(node, path, isPathToLeafProperty ? ext : null);
-    // attachments = node.attachments
-    // attachments.map( item =>
-    //     console.log(item.fileUrl))
-
-    // grab the parent of the leaf property, if it's not the current value of `node` already
-    // ex: `parentNode` in `myNodes[].parentNode.leafProperty`
-    let nextNode = node;
-    if (isPathToLeafProperty && path.includes('.')) {
-        const pathToLastParent = path
-            .split('.')
-            .slice(0, -1)
-            .join('.');
-        nextNode = get(node, pathToLastParent);
-    }
-    // console.log(nextNode)
-    return Array.isArray(nextValue)
-        ? // Recursively call function with next path segment for each array element
-        Promise.all(
-            nextValue.map(item =>
-                createImageNodesInArrays(
-                    imagePathSegments[pathIndex + 1],
-                    item,
-                    options
-                ),
-                // console.log("running function")
-            ),
-        )
-        : // otherwise, handle leaf node
-        createImageNode(nextValue, nextNode, options);
+    projectPosts.forEach((n, index) => {
+        const next = index === 0 ? null : projectPosts[index - 1]
+        const prev = index === projectPosts.length - 1 ? null : projectPosts[index + 1]
+        if (n.node.fileAbsolutePath.indexOf('hiring') >= 0) {
+            projectTemplate = require.resolve('./src/templates/hiringTemplate.js');
+            slug = "/hiring/" + n.node.frontmatter.title.toLowerCase().split(" ").join("_");
+        }
+        else if (n.node.fileAbsolutePath.indexOf('groups') >= 0) {
+            projectTemplate = require.resolve('./src/templates/groupsTemplate.js');
+            slug = "/groups/" + n.node.frontmatter.title.toLowerCase().split(" ").join("_");;
+        }
+        else {
+            projectTemplate = require.resolve('./src/templates/aboutTemplate.js');
+            slug = n.node.frontmatter.title.toLowerCase().split(" ").join("_");
+        }
+        createPage({
+            path: slug,
+            component: projectTemplate,
+            context: {
+                slug: slug,
+                date: n.node.frontmatter.date,
+                absolutePathRegex: n.node.fileAbsolutePath,
+            },
+        })
+    })
 }
---
+++ src/templates/hiringTemplate.js
@@ -0,0 +1,299 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import styled from 'styled-components';
+import BackgroundImg from 'gatsby-background-image';
+import { ExpandMoreRounded } from '@material-ui/icons';
+import { Clear, KeyboardArrowLeft, KeyboardArrowRight } from '@material-ui/icons';
+import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
+import Img from 'gatsby-image'
+
+
+export default class BlogTemplate extends React.Component {
+    state = {
+        showLightbox: false,
+        selectedImage: 0,
+        gallerylength: 0,
+    }
+
+    componentDidMount = () => {
+        this.retreiveGalleryLength()
+        window.addEventListener('keyup', this.handleKeyUp, false)
+        this.targetElement = document.querySelector('#targetElementId');
+
+    }
+
+    componentWillUnmount = () => {
+        window.removeEventListener('keyup', this.handleKeyUp, false)
+        clearAllBodyScrollLocks();
+    }
+
+    retreiveGalleryLength = () => {
+        this.props.data.allMarkdownRemark.edges.map(
+            (post, i) => {
+                this.setState(
+                    {
+                        gallerylength: post.node.frontmatter.gallery.length
+                    }
+                );
+            }
+        );
+    };
+
+    handleClick = (e, index) => {
+        e.preventDefault()
+        this.setState({ showLightbox: !this.state.showLightbox, selectedImage: index });
+        disableBodyScroll(this.targetElement);
+    };
+
+    closeModal = () => {
+        this.setState({ showLightbox: false })
+        enableBodyScroll(this.targetElement);
+    }
+
+    goBack = () => {
+        if (this.state.selectedImage > 0) {
+            this.setState({ selectedImage: this.state.selectedImage - 1 })
+        }
+    }
+
+    goForward = () => {
+        if (this.state.selectedImage < this.state.gallerylength - 1) {
+            this.setState({ selectedImage: this.state.selectedImage + 1 })
+        }
+    }
+
+    handleKeyUp = e => {
+        e.preventDefault()
+        const { keyCode } = e
+        if (this.state.showLightbox) {
+            if (keyCode === 37) {
+                // Left Arrow Key
+                if (this.state.selectedImage > 0) {
+                    this.setState({ selectedImage: this.state.selectedImage - 1 })
+                }
+            }
+            if (keyCode === 39) {
+                // Right Arrow Key
+                if (this.state.selectedImage < this.state.gallerylength - 1) {
+                    this.setState({ selectedImage: this.state.selectedImage + 1 })
+                }
+            }
+            if (keyCode === 27) {
+                // Escape key
+                this.setState({ showLightbox: false })
+                enableBodyScroll(this.targetElement);
+            }
+        }
+    }
+
+    render() {
+        console.log("testing")
+        const { showLightbox, selectedImage } = this.state
+        return (
+            <Layout>
+                <Wrapper>
+                    {this.props.data.allMarkdownRemark.edges.map(
+                        (post, i) => {
+                            const images = post.node.frontmatter.gallery
+                            return (
+                                <div>
+                                    <ImgWrapper>
+                                        <BackgroundImg key={post.node.frontmatter.cover.childImageSharp.id} fadeIn={true} style={{ height: "50vh", borderRadius: "20px" }} fluid={post.node.frontmatter.cover.childImageSharp.fluid}>
+                                            <Hero>
+                                                <TitleWrapper>
+                                                    {post.node.frontmatter.title}
+                                                    <ExpandMoreRounded style={{ display: "block", fontSize: "1em", textAlign: "center", width: "98%" }} />
+                                                </TitleWrapper>
+                                            </Hero>
+                                        </BackgroundImg>
+                                    </ImgWrapper>
+                                    <Gallery>
+                                        {post.node.frontmatter.gallery.map((img, i) => {
+                                            return (
+                                                <GalleryItem key={img.image.childImageSharp.id}>
+                                                    <a href={img.image.childImageSharp.fluid.src} onClick={e => this.handleClick(e, i)}>
+                                                        <Img fluid={img.image.childImageSharp.fluid} />
+                                                    </a>
+                                                </GalleryItem>
+                                            )
+                                        })}
+                                    </Gallery>
+                                    <LightboxModal visible={showLightbox} onKeyUp={e => this.handleKeyDown(e)}>
+                                        <LightboxContent>
+                                            <Img imgStyle={{ objectFit: 'contain' }} fluid={images[selectedImage].image.childImageSharp.fluid} style={{ maxHeight: "80vh", maxWidth: "80vw" }} />
+                                            <Controls>
+                                                <KeyboardArrowLeft onClick={this.goBack} disabled={selectedImage === 0} style={{ display: "block", color: "white", fontSize: "50px" }} />
+                                                <Clear onClick={this.closeModal} style={{ display: "block", color: "white", fontSize: "50px" }} />
+                                                <KeyboardArrowRight onClick={this.goForward} disabled={selectedImage === images.length - 1} style={{ display: "block", color: "white", fontSize: "50px" }} />
+                                            </Controls>
+                                        </LightboxContent>
+                                    </LightboxModal>
+                                    <ContentWrapper>
+                                        <Date>{post.node.frontmatter.date}</Date>
+                                        <Content
+                                            dangerouslySetInnerHTML={{ __html: post.node.html }}
+                                        />
+                                    </ContentWrapper>
+                                </div>
+                            )
+                        }
+                    )}
+                </Wrapper>
+            </Layout>
+        )
+    }
+}
+
+export const pageQuery = graphql`
+query($absolutePathRegex: String!)
+{
+  allMarkdownRemark(filter: {fileAbsolutePath: {eq: $absolutePathRegex}}) {
+    edges {
+      node {
+        id
+        html
+        frontmatter {
+          date(formatString: "DD-MM-YYYY")
+          title
+          gallery {
+            image {
+              absolutePath
+              id
+              name
+              childImageSharp {
+                fluid {
+                  base64
+                  tracedSVG
+                  aspectRatio
+                  src
+                  srcSet
+                  srcWebp
+                  srcSetWebp
+                  sizes
+                  originalImg
+                  originalName
+                  presentationWidth
+                  presentationHeight
+                }
+              }
+            }
+          }
+          cover {
+            id
+            name
+            childImageSharp {
+              fluid {
+                base64
+                tracedSVG
+                aspectRatio
+                src
+                srcSet
+                srcWebp
+                srcSetWebp
+                sizes
+                originalImg
+                originalName
+                presentationWidth
+                presentationHeight
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+`
+
+const Wrapper = styled.div`
+  display: relative;
+  height: auto;
+  padding: 20px;
+  width: 70%;
+  padding-left: 15%;
+`
+
+const ImgWrapper = styled.div`
+  display: relative;
+  height: 50vh;
+  padding: 20px;
+`
+
+const TitleWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 70px;
+  top: 60%;
+  text-align: center;
+  color: white;
+  font-family: 'Concert One', cursive;
+  font-size: 5em;
+  background-color: #62EDD6;
+  border-radius: 20px;
+  @media (max-width: 600px) {
+    font-size: 3em;
+  }
+`
+
+const ContentWrapper = styled.div`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 30px;
+`
+
+const Content = styled.div`
+  flex: 1 0 auto;
+`
+
+const Hero = styled.div`
+  display: block;
+  height: 100%;
+  font-size: .5em;
+  padding: 20px;
+`
+const Date = styled.div`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 20px;
+`
+const Gallery = styled.div`
+  width: 100%;
+  min-height: 50px;
+  border-radius: 20;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`
+
+const GalleryItem = styled.div`
+  padding: 10px;
+  min-width: 100px;
+  width: 15%;
+  flex-shrink: 1;
+  border-radius: 20;
+`
+
+const LightboxModal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.8);
+  opacity: ${props => (props.visible ? '1' : '0')};
+  visibility: ${props => (props.visible ? 'visible' : 'hidden')};
+`
+const LightboxContent = styled.div`
+  margin: 30px;
+  max-width: 1500px;
+  width: 100%;
+`
+
+const Controls = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+`
